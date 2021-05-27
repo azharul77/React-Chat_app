80cd9f6aa3481955a46e447aac96e73551482496
azharul77 <suvromulla921@gmail.com>
2021-05-28 01:27:43 +0600

    chat app uploding in git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
-function App() {
+const App = () => {
+  if (!localStorage.getItem("User-Name")) return <LoginForm />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="2a311c14-7f95-4b04-a496-b174b852e0b7"
+      userName={localStorage.getItem("User-Name", "username")}
+      userSecret={localStorage.getItem("User-Secret", "password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
-}
+};
 
 export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = {
+      "Project-ID": "2a311c14-7f95-4b04-a496-b174b852e0b7",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+    try {
+        await axios.get("https://api.chatengine.io/chats", { headers: authObject });
+      localStorage.setItem("User-Name", username);
+      localStorage.setItem("User-Secret", password);
+
+      window.location.reload();
+    } catch (error) {
+      setError("Oops, incorrect credentials.");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Log In</span>
+            </button>
+          </div>
+          <h2 className="error">{error}</h2>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
